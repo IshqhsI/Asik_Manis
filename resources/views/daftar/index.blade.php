f183728a1914493fb5ee2ea1522e3d05be8ffe5d
IshqhsI <oneridh18@github.com>
2022-11-28 19:39:59 +0700

    MineeeNew

--- resources/views/daftar/index.blade.php
+++ resources/views/daftar/index.blade.php
@@ -168,7 +168,6 @@
                                 </form>
                                 <br>
                             </div>
-
                         </div>
                     </div>
                 </div>
